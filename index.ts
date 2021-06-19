d0e4b773148f6c6614229ab279eaf6dd9897728b
fantasim <siuol@gmx.com>
2021-06-19 06:41:13 +0300

    1.0.1

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import * as Constant from './src/constant'
 import ScriptEngine from './src/engine'
 import * as Opcode from './src/opcode'
 import * as Constitution from './src/constitution'
+import * as Error from './src/errors'
 
 export {
     Constant,
     Opcode,
     ScriptEngine,
-    Constitution
+    Constitution,
+    Error
 }
