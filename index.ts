f2b47c56a19032a33ff83b9686d848e368d278a9
fantasim <siuol@gmx.com>
2022-08-27 16:26:11 +0300

    export++

--- index.ts
+++ index.ts
@@ -3,10 +3,14 @@ import * as Constant from './src/constant'
 import Script from './src/script'
 import * as Constitution from './src/constitution'
 import * as Error from './src/errors'
+import * as ContentCode from './src/content-code'
+import * as OpCode from './src/opcode'
 
 export {
     Constant,
     Script,
     Constitution,
-    Error
+    Error,
+    ContentCode,
+    OpCode
 }
